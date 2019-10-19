19ea0b56ced69c8c1a80ccde3d321f1f022fd005
kapil garg <kapilgarg@kapils-MacBook-Pro.local>
2019-10-19 18:15:02 +0530

    picking mongo db url using environment variable

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect('mongodb://iamkapilgarg:kapil7030@cluster0-shard-00-00-zzlbn.mongodb.net:27017,cluster0-shard-00-01-zzlbn.mongodb.net:27017,cluster0-shard-00-02-zzlbn.mongodb.net:27017/task-manager-app?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin', {
-    //mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
